73a8b605c41b91d8447e9c0c95a3bd7061e0f4b1
Kevin De Silva <kevin.desilva@santander.co.uk>
2025-04-15 17:34:21 +0100

    feat: initial stuff again lol

--- main.py
+++ main.py
@@ -2,4 +2,12 @@
 
 # Want to visualise the Win Triangle for a given box setup of 2 by n
 
+# n=columns
+def alice(n):
+    arr=[x+1 for x in range(2*n)]
+    return arr
 
+def don(n):
+    arr=[if(x%2==0):x for x in range(2*n)]
+
+print(alice(4))
